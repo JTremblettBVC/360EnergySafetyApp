e53c3a1c58c24a2f0038137f5e6effae28249c68
oxanabelokon <oksibelok@gmail.com>
2023-01-30 19:18:23 -0700

    FirstDayonSite Activity added

---
+++ app/src/main/java/com/example/a360energysafetyapp/FirstDayOnSite.java
@@ -0,0 +1,14 @@
+package com.example.a360energysafetyapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FirstDayOnSite extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_day_on_site);
+    }
+}
