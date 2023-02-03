465381b0ad72e02362a3d23a147aeaecae14b74e
María <98559654+mlestrella843@users.noreply.github.com>
2023-02-02 17:35:20 -0700

    Adding functionalities to Forms

---
+++ app/src/main/java/com/example/a360energysafetyapp/ATV_OHVInspection.java
@@ -0,0 +1,14 @@
+package com.example.a360energysafetyapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ATV_OHVInspection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_atv_ohvinspection);
+    }
+}
